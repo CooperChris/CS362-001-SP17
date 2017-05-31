7af5f999bcad44dc0b6dab25de8ec8733f276f09
Christopher Cooper <coopchri@flip3.engr.oregonstate.edu>
2017-05-30 22:46:52 -0700

    Finished tests for branch coverage

--- projects/coopchri/assignment-4/Calendar/src/test/java/edu/osu/cs362/ApptRandomTest.java
+++ projects/coopchri/assignment-4/Calendar/src/test/java/edu/osu/cs362/ApptRandomTest.java
@@ -15,78 +15,117 @@ import static org.junit.Assert.*;
  */
 
 public class ApptRandomTest {
-	private static final long TestTimeout = 2 * 60 * 500 * 1; /* Timeout at 30 seconds */
-	private static final int NUM_TESTS=100;
+   private static final long TestTimeout = 2 * 60 * 500 * 1; /* Timeout at 30 seconds */
+   private static final int NUM_TESTS=100;
 
-	/**
-	 * Return a randomly selected method to be tests !.
-	 */
-    public static String RandomSelectMethod(Random random){
-        String[] methodArray = new String[] {"isValid","setDescription"};// The list of the of methods to be tested in the Appt class
+   /**
+    * Return a randomly selected method to be tests !.
+    */
+   public static String RandomSelectMethod(Random random){
+      String[] methodArray = new String[] {"isValid","setDescription"};// The list of the of methods to be tested in the Appt class
 
-    	int n = random.nextInt(methodArray.length);// get a random number between 0 (inclusive) and  methodArray.length (exclusive)
-    	            
-        return methodArray[n] ; // return the method name 
-        }
-	
-    /**
-     * Generate Random Tests that tests Appt Class.
-     */
-	 @Test
-	  public void radnomtest()  throws Throwable  {
+      int n = random.nextInt(methodArray.length);// get a random number between 0 (inclusive) and  methodArray.length (exclusive)
 
-		 long startTime = Calendar.getInstance().getTimeInMillis();
-		 long elapsed = Calendar.getInstance().getTimeInMillis() - startTime;
+      return methodArray[n] ; // return the method name 
+   }
 
-		 
-		 System.out.println("Start testing...");
-		 
-		 
-			for (int iteration = 0; elapsed < TestTimeout; iteration++) {
-				long randomseed =10;//System.currentTimeMillis();
-	//			System.out.println(" Seed:"+randomseed );
-				Random random = new Random(randomseed);
-				
-				 int startHour=13;
-				 int startMinute=30;
-				 int startDay=10;
-				 int startMonth=4;
-				 int startYear=2017;
-				 String title="Birthday Party";
-				 String description="This is my birthday party.";
-				 //Construct a new Appointment object with the initial data	 
-				 Appt appt = new Appt(startHour,
-				          startMinute ,
-				          startDay ,
-				          startMonth ,
-				          startYear ,
-				          title,
-				         description);
-				for (int i = 0; i < NUM_TESTS; i++) {
-					String methodName = ApptRandomTest.RandomSelectMethod(random);
-					   if (methodName.equals("Valid")){
-   						   int hour=(int) ValuesGenerator.getRandomIntBetween(random, -10, 27);
-						   appt.setStartHour(hour);						   
-	   					   int minute=(int) ValuesGenerator.getRandomIntBetween(random, -10, 70);
-						   appt.setStartMinute(minute);						   
-		   				   int day=(int) ValuesGenerator.getRandomIntBetween(random, -10, 40);
-						   appt.setStartDay(day);						   
-			   			   int month=(int) ValuesGenerator.getRandomIntBetween(random, -10, 20);
-						   appt.setStartMonth(month);						   
-			}
-					
-				}
-				
-				 elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
-			        if((iteration%10000)==0 && iteration!=0 )
-			              System.out.println("elapsed time: "+ elapsed + " of "+TestTimeout);
-			 
-			}
-	 
-	 
-		 System.out.println("Done testing...");
-	 }
+   /**
+    * Generate Random Tests that tests Appt Class.
+    */
+   @Test
+   public void radnomtest()  throws Throwable  {
+
+      long startTime = Calendar.getInstance().getTimeInMillis();
+      long elapsed = Calendar.getInstance().getTimeInMillis() - startTime;
 
 
-	
+      System.out.println("Start testing...");
+
+
+      for (int iteration = 0; elapsed < TestTimeout; iteration++) {
+         long randomseed =10;//System.currentTimeMillis();
+         //			System.out.println(" Seed:"+randomseed );
+         Random random = new Random(randomseed);
+
+         int startHour=13;
+         int startMinute=30;
+         int startDay=10;
+         int startMonth=4;
+         int startYear=2017;
+         String title="Birthday Party";
+         String description=ValuesGenerator.getString(random);
+         //Construct a new Appointment object with the initial data	 
+         Appt appt = new Appt(startHour,
+               startMinute ,
+               startDay ,
+               startMonth ,
+               startYear ,
+               title,
+               description);
+         if ((iteration % 50) == 0){
+            appt.setDescription(null);
+         }
+         for (int i = 0; i < NUM_TESTS; i++) {
+            String methodName = ApptRandomTest.RandomSelectMethod(random);
+            if (methodName.equals("isValid")){
+               int hour=(int) ValuesGenerator.getRandomIntBetween(random, 0, 23);
+               appt.setStartHour(hour);						   
+               int minute=(int) ValuesGenerator.getRandomIntBetween(random, 0, 59);
+               appt.setStartMinute(minute);						   
+               int day=(int) ValuesGenerator.getRandomIntBetween(random, 1, 31);
+               appt.setStartDay(day);						   
+               int month=(int) ValuesGenerator.getRandomIntBetween(random, 1, 12);
+               appt.setStartMonth(month);
+               //change to invalid hours
+               hour=(int) ValuesGenerator.getRandomIntBetween(random, -10, -1);
+               appt.setStartHour(hour);
+               hour=(int) ValuesGenerator.getRandomIntBetween(random, 0, 23);
+               appt.setStartHour(hour);
+               hour=(int) ValuesGenerator.getRandomIntBetween(random, 24, 30);
+               appt.setStartHour(hour);
+               hour=(int) ValuesGenerator.getRandomIntBetween(random, 0, 23);
+               appt.setStartHour(hour);
+               //change to invalid minutes
+               minute=(int) ValuesGenerator.getRandomIntBetween(random, -20, -1);
+               appt.setStartMinute(minute);
+               minute=(int) ValuesGenerator.getRandomIntBetween(random, 0, 59);
+               appt.setStartMinute(minute);
+               minute=(int) ValuesGenerator.getRandomIntBetween(random, 60, 70);
+               appt.setStartMinute(minute);
+               minute=(int) ValuesGenerator.getRandomIntBetween(random, 0, 59);
+               appt.setStartMinute(minute);
+               //change to invalid days
+               day=(int) ValuesGenerator.getRandomIntBetween(random, -10, 0);
+               appt.setStartDay(day);
+               day=(int) ValuesGenerator.getRandomIntBetween(random, 1, 31);
+               appt.setStartDay(day);
+               day=(int) ValuesGenerator.getRandomIntBetween(random, 32, 40);
+               appt.setStartDay(day);
+               day=(int) ValuesGenerator.getRandomIntBetween(random, 1, 31);
+               appt.setStartDay(day);
+               //change to invalid months
+               month=(int) ValuesGenerator.getRandomIntBetween(random, -10, 0);
+               appt.setStartMonth(month);						   
+               month=(int) ValuesGenerator.getRandomIntBetween(random, 1, 12);
+               appt.setStartMonth(month);
+               month=(int) ValuesGenerator.getRandomIntBetween(random, 12, 20);
+               appt.setStartMonth(month);
+               month=(int) ValuesGenerator.getRandomIntBetween(random, 1, 12);
+               appt.setStartMonth(month);
+            }
+
+         }
+
+         elapsed = (Calendar.getInstance().getTimeInMillis() - startTime);
+         if((iteration%10000)==0 && iteration!=0 )
+            System.out.println("elapsed time: "+ elapsed + " of "+TestTimeout);
+
+      }
+
+
+      System.out.println("Done testing...");
+   }
+
+
+
 }
--- projects/coopchri/assignment-4/Calendar/src/test/java/edu/osu/cs362/CalDayRandomTest.java
+++ projects/coopchri/assignment-4/Calendar/src/test/java/edu/osu/cs362/CalDayRandomTest.java
@@ -15,60 +15,52 @@ import static org.junit.Assert.*;
  */
 
 public class CalDayRandomTest {
-	
-    /**
-     * Generate Random Tests that tests CalDay Class.
-     */
-	 @Test
-	  public void radnomtest()  throws Throwable  {
-                Random rnd = new Random(100);
-		GregorianCalendar cal = new GregorianCalendar();
-                CalDay day = new CalDay(cal);
-                //create the value generator and a appt
-                //ValuesGenerator gen = new ValuesGenerator();
 
-                //create variables for hour, minute and counter of appts
-                int hour, min, count = 0;
-                //start a loop to change values and begin adding appointments
-                for (int i = 0; i < 1000; i++){
-                     Appt appt = new Appt(12, 30, cal.get(cal.DAY_OF_MONTH), cal.get(cal.MONTH), cal.get(cal.YEAR), "Birthday", "birthday");
+   /**
+    * Generate Random Tests that tests CalDay Class.
+    */
+   @Test
+   public void radnomtest()  throws Throwable  {
+      Random rnd = new Random(100);
+      GregorianCalendar cal = new GregorianCalendar();
+      CalDay day = new CalDay(cal);
+      //create the value generator and a appt
+      //ValuesGenerator gen = new ValuesGenerator();
 
-                    //check if appt is valid
-                     if (appt.getValid() == true){
-                        //add to counter
-                        count = count + 1;
-                     }
-                     //add appt to day
-                     day.addAppt(appt);
+      //create variables for hour, minute and counter of appts
+      int hour = 0, min = 0;
+      //start a loop to change values and begin adding appointments
+      for (int i = 0; i < 2000; i++){
+         Appt appt = new Appt(12, 30, 
+               cal.get(cal.DAY_OF_MONTH), 
+               cal.get(cal.MONTH), 
+               cal.get(cal.YEAR), 
+               "Birthday", "birthday");
 
-                     //check that size of day appts is same as count
-                     assertEquals(count, day.getSizeAppts());
+         if ((i%100) == 0){
+         //change values for start hour and minute
+         appt.setStartHour((int)ValuesGenerator.getRandomIntBetween(rnd, 25, 30));
+         appt.setStartMinute((int)ValuesGenerator.getRandomIntBetween(rnd, 60, 70));
+         }
+         else{
+       //change values for start hour and minute
+         appt.setStartHour((int)ValuesGenerator.getRandomIntBetween(rnd, 12, 23));
+         appt.setStartMinute((int)ValuesGenerator.getRandomIntBetween(rnd, 30, 59));
+         }
+         //add appt to day
+         day.addAppt(appt);
+      }
+      for (int i = 0; i < 2000; i++){
+         Appt appt = new Appt(12, 30, cal.get(cal.DAY_OF_MONTH), cal.get(cal.MONTH), cal.get(cal.YEAR), "Birthday", "birthday");
 
-                     //change values for start hour and minute
-                     appt.setStartHour((int)ValuesGenerator.getRandomIntBetween(rnd, 12, 24));
-                     appt.setStartMinute((int)ValuesGenerator.getRandomIntBetween(rnd, 30, 60));
-                }
-                for (int i = 0; i < 1000; i++){
-                     Appt appt = new Appt(12, 30, cal.get(cal.DAY_OF_MONTH), cal.get(cal.MONTH), cal.get(cal.YEAR), "Birthday", "birthday");
-
-//check if appt is valid
-                     if (appt.getValid() == true){
-                        //add to counter
-                        count = count + 1;
-                     }
-                     //add appt to day
-                     day.addAppt(appt);
-
-                     //check that size of day appts is same as count
-                     assertEquals(count, day.getSizeAppts());
-
-                     //change values for start hour and minute
-                     appt.setStartHour((int)ValuesGenerator.getRandomIntBetween(rnd, -1, 12));
-                     appt.setStartMinute((int)ValuesGenerator.getRandomIntBetween(rnd, -1, 30));
-                } 
-
-	 }
+         //change values for start hour and minute
+         appt.setStartHour((int)ValuesGenerator.getRandomIntBetween(rnd, 0, 12));
+         appt.setStartMinute((int)ValuesGenerator.getRandomIntBetween(rnd, 0, 30));
+          //add appt to day
+         day.addAppt(appt);
+      } 
+   }
 
 
-	
+
 }
